44b56196f4ee31452cdea64aab2190a2719d667c
Crew Spence <csspence123@gmail.com>
2020-07-07 07:18:46 -0500

    Wrote working solution.

--- factorial_udemy.js
+++ factorial_udemy.js
@@ -5,8 +5,14 @@ factorial
 Write a function factorial which accepts a number and returns the factorial of that number.  A factorial is the product of an integer and all the integers below it; e.g., factor four (4!) is equal to 24, because 4 * 3 * 2 * 1 equals 24.  factorial zero (0!) is always 1.
 */
 
-function factorial(){
-   
+const factorial = (num) => {
+  if(num === 0) {
+    return 1;
+  }
+  if(num === 1) {
+    return num;
+  }
+  return num * factorial(num - 1);
 }
 
 //factorial(1) // 1
