edb9aab6e5e2925da663a1d9e9293f8b05ac161e
Crew Spence <csspence123@gmail.com>
2020-07-07 07:09:08 -0500

    Created function stub for toy problem.

---
+++ factorial_udemy.js
@@ -0,0 +1,15 @@
+/*
+JavaScript Algorithms and Data Structures Masterclass
+
+factorial
+Write a function factorial which accepts a number and returns the factorial of that number.  A factorial is the product of an integer and all the integers below it; e.g., factor four (4!) is equal to 24, because 4 * 3 * 2 * 1 equals 24.  factorial zero (0!) is always 1.
+*/
+
+function factorial(){
+   
+}
+
+//factorial(1) // 1
+// factorial(2) // 2
+// factorial(4) // 24
+// factorial(7) // 5040
